f806709a3b0e9995ad08dc53955c6d4fd4de9a30
Madhav Bhalani <madhavbhalani@gmail.com>
2022-07-28 15:27:25 +0530

    added recycler view

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.DataSource
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,11 @@ class MainActivity : AppCompatActivity() {
 
 //        val textView: TextView = findViewById(R.id.textview)
 //        textView.text = DataSource().loadAffirmations().size.toString()
+        val myDataSet = DataSource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+
+        recyclerView.setHasFixedSize(true)
     }
 }
